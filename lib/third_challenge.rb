cabefc557be18fdb671fb6a0fb0d8cce8d6f47aa
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-14 17:00:15 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,8 +11,8 @@ def third_challenge
     },
       :hero =>{
         :name => "Romeo",
-        :age => "15"
-        :status => "alive",
+        :age => "15",
+        :status => "alive"
       },
       :hero_friends => [
       {
